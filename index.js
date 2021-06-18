00e559f02bc698a4764ea4627f058346d6806d78
William Martin <contact@willmartin.dev>
2021-06-18 10:15:52 -0400

    🚀 🚀 🚀

---
+++ index.js
@@ -0,0 +1,32 @@
+// document.addEventListener('DOMContentLoaded', () => document.querySelectorAll('div[is]').forEach(el => {
+//   try {
+//     const newEl = document.createElement(el.getAttribute('is'));
+//     [...el.attributes].forEach(at => newEl.setAttribute(at.name, at.value));
+//     newEl.innerHTML = el.innerHTML;
+//     el.replaceWith(newEl);
+//   } catch (error) {
+//     console.error(`Cannot convert element: ${el.outerHTML}`);
+//   }
+// }));
+
+document.addEventListener('DOMContentLoaded', () => {
+  let els = document.querySelectorAll('div[is]');
+  do {
+    swapDivs(els);
+    els = document.querySelectorAll('div[is]');
+  } while (els.length > 0);
+});
+
+const swapDivs = (els) => {
+  console.log('Bye bye divs! 🎉');
+  els.forEach(el => {
+    try {
+      const newEl = document.createElement(el.getAttribute('is'));
+      [...el.attributes].forEach(at => newEl.setAttribute(at.name, at.value));
+      newEl.innerHTML = el.innerHTML;
+      el.replaceWith(newEl);
+    } catch (error) {
+      console.error(`Cannot convert element: ${el.outerHTML}`);
+    }
+  })
+}
